89968e7b3b73dc02e6a132574d5c5f8c83afc12f
Hubert Kalinowski <kalinows_1167553@spk-ssh.if.uj.edu.pl>
2020-06-03 21:29:10 +0200

    formatowanie

--- Zestaw08/wykluczanie.c
+++ Zestaw08/wykluczanie.c
@@ -8,14 +8,14 @@
 int licznik = 0;
 void *thread(void *arg);
 pthread_mutex_t mutex;
-struct thread_arg//argument dla wątku
-{                      
-    pthread_t id;      //id wątku (od pthread_create)
-    int num;           //numer wątku
+struct thread_arg //argument dla wątku
+{
+    pthread_t id; //id wątku (od pthread_create)
+    int num;      //numer wątku
 };
 int main(int argc, char const *argv[])
 {
-pthread_mutex_init(&mutex,NULL);
+    pthread_mutex_init(&mutex, NULL);
     struct thread_arg thTab[LW];
     printf("\033c\n");
     for (int i = 0; i < LW; i++)
@@ -30,32 +30,32 @@ pthread_mutex_init(&mutex,NULL);
 
     for (int i = 0; i < LW; i++)
     {
-        if(pthread_join(thTab[i].id,NULL)!=0){
+        if (pthread_join(thTab[i].id, NULL) != 0)
+        {
             printf("pthread join error\n");
             exit(1);
         }
-        
     }
-    printf("\033[0m\033[%d;%dH\n",LW,0);
+    printf("\033[0m\033[%d;%dH\n", LW, 0);
     return 0;
 }
 void *thread(void *arg)
 {
-    struct thread_arg *tmp=arg;
+    struct thread_arg *tmp = arg;
     int l;
-    for(int i=0;i<10;i++)
+    for (int i = 0; i < 10; i++)
     {
-    printf("\033[0m\033[%d;%dHpthread(%d)\033[0m\n", tmp->num, 0,tmp->num);
-    pthread_mutex_lock(&mutex);
-    l=licznik;
-    printf("\033[%d;%dH(%d/%d)\n", tmp->num, 12,l,licznik);
-l++;
-    printf("\033[33m\033[%d;%dH(%d/\033[31m%d)\033[0m\n", tmp->num, 12,l,licznik);
-    usleep(100000);
-    licznik=l;
-    printf("\033[%d;%dH(%d/\033[31m%d)\033[0m\n", tmp->num, 12,l,licznik);
-    pthread_mutex_unlock(&mutex);
+        printf("\033[0m\033[%d;%dHpthread(%d)\033[0m\n", tmp->num, 0, tmp->num);
+        pthread_mutex_lock(&mutex);
+        l = licznik;
+        printf("\033[%d;%dH(%d/%d)\n", tmp->num, 12, l, licznik);
+        l++;
+        printf("\033[33m\033[%d;%dH(%d/\033[31m%d)\033[0m\n", tmp->num, 12, l, licznik);
+        usleep(100000);
+        licznik = l;
+        printf("\033[%d;%dH(%d/\033[31m%d)\033[0m\n", tmp->num, 12, l, licznik);
+        pthread_mutex_unlock(&mutex);
     }
-    printf("\033[32m\033[%d;%dH(%d/%d)\033[0m\n", tmp->num, 12,l,licznik);
+    printf("\033[32m\033[%d;%dH(%d/%d)\033[0m\n", tmp->num, 12, l, licznik);
     return NULL;
 }
